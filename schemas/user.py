0ee2b135390368bf7ce175e828c1145f66f43959
maruf ibragimov <ibragimov.maruf@gmail.com>
2025-02-23 21:39:10 +0500

    add user schema

---
+++ schemas/user.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class UserSchema(BaseModel):
+    full_name: str
+    username: str
+    password: str
+
+
+class UserSignInSchema(BaseModel):
+    username: str
+    password: str
